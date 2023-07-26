9acc11a2f3ad818c4ae4314f22e8d480c05944ae
Valentin Madrid <94240868+valentinmadrid@users.noreply.github.com>
2023-07-27 00:41:25 +0200

    Revert "Redesign of the Devnet Faucet."

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,21 @@
 "use client";
-
-import React, { useState, ChangeEvent, useEffect } from "react";
+import React, { useState, ChangeEvent, useCallback, use } from "react";
 import { PublicKey } from "@solana/web3.js";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Coins } from "lucide-react";
-import { Input } from "@/components/ui/input";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import Turnstile from "react-turnstile";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Header } from "@/components/header";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Toaster } from "@/components/ui/toaster";
-import { useToast } from "@/components/ui/use-toast";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+
+const amountOptions = [0.5, 1, 2, 5];
 
 export default function Home() {
-  const amountOptions = [0.5, 1, 2.5, 5];
   const [walletAddress, setWalletAddress] = useState<string>("");
   const [amount, setAmount] = useState<number | null>(null);
   const [errors, setErrors] = useState<{ wallet: string; amount: string }>({
     wallet: "",
     amount: "",
   });
-  const [showVerifyDialog, setShowVerifyDialog] = useState<boolean>(false);
-  const toaster = useToast();
+  const [cloudflareCallback, setCloudflareCallback] = useState<string>("");
+  const [hasRequested, setHasRequested] = useState<boolean>(false);
 
   const validateWallet = (address: string): boolean => {
     try {
@@ -70,15 +43,36 @@ export default function Home() {
     }
   };
 
-  const requestAirdrop = async (cloudflareCallback: string | null = null) => {
-    try {
-      if (cloudflareCallback === null) {
-        toaster.toast({
-          title: "Error!",
-          description: "Please complete the captcha.",
-        });
-      }
+  const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const amount = Number(event.target.value);
+    setAmount(amount);
 
+    if (amount > 5) {
+      setErrors((errors) => ({
+        ...errors,
+        amount: "Amount cannot be more than 5",
+      }));
+    } else {
+      setErrors((errors) => ({
+        ...errors,
+        amount: "",
+      }));
+    }
+  };
+
+  const isFormValid = (): boolean => {
+    return (
+      walletAddress !== "" &&
+      amount !== null &&
+      amount <= 5 &&
+      cloudflareCallback !== "" &&
+      errors.wallet === "" &&
+      errors.amount === ""
+    );
+  };
+
+  const requestAirdrop = async () => {
+    try {
       const res = await fetch("/api/request", {
         method: "POST",
         headers: {
@@ -88,123 +82,34 @@ export default function Home() {
       });
 
       if (res.ok) {
-        toaster.toast({
-          title: "Success!",
-          description: "Airdrop was successfull.",
+        toast("Airdrop successful", {
+          hideProgressBar: true,
+          autoClose: 4000,
+          type: "success",
         });
+        setHasRequested(true);
       } else {
         const data = await res.json();
 
-        toaster.toast({
-          title: "Error!",
-          description: `${data.error}`,
+        toast(`${data.error}`, {
+          hideProgressBar: true,
+          autoClose: 4000,
+          type: "error",
         });
       }
     } catch (err) {
-      toaster.toast({
-        title: "Error!",
-        description: `Failed to request airdrop, error: ${err}`,
+      toast(`Failed to request airdrop, error: ${err}`, {
+        hideProgressBar: true,
+        autoClose: 2000,
+        type: "error",
       });
     }
   };
 
-  const [isFormValid, setIsFormValid] = useState(false);
-
-  useEffect(() => {
-    console.log({
-      walletAddress,
-      amount,
-      errorsWallet: errors.wallet,
-      errorsAmount: errors.amount,
-    });
-    setIsFormValid(
-      walletAddress !== "" &&
-        amount !== null &&
-        amount <= 5 &&
-        errors.wallet === "" &&
-        errors.amount === ""
-    );
-  }, [amount, walletAddress]);
-
   return (
-    <div className="min-h-screen relative flex items-center justify-center">
-      <Toaster />
-
-      <Header />
-
-      <div className="absolute">
-        <Card className="w-[450px]">
-          <CardHeader>
-            <CardTitle>Request Airdrop</CardTitle>
-            <CardDescription>Maximum of 2 requests per hour.</CardDescription>
-          </CardHeader>
-
-          <CardContent className="flex flex-row space-x-2">
-            <Input
-              placeholder="Wallet Address"
-              onChange={handleWalletChange}
-              value={walletAddress}
-            />
-
-            <Popover>
-              <PopoverTrigger>
-                <Button className="w-24" variant="outline">
-                  {amount ? amount + " SOL" : "Amount"}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-32 grid grid-cols-2 gap-2">
-                {amountOptions.map((option) => (
-                  <Button variant="outline" onClick={() => setAmount(option)}>
-                    {option}
-                  </Button>
-                ))}
-              </PopoverContent>
-            </Popover>
-          </CardContent>
-
-          <CardFooter>
-            <Dialog
-              open={showVerifyDialog}
-              onOpenChange={(open: boolean) => setShowVerifyDialog(open)}
-            >
-              <Button
-                className="w-full"
-                variant="default"
-                disabled={!isFormValid}
-                onClick={() => setShowVerifyDialog(true)}
-              >
-                <Coins className="mr-2 h-4 w-4" /> Confirm Airdrop
-              </Button>
-
-              <DialogContent className="max-w-[450px]">
-                <DialogHeader>
-                  <DialogTitle>Cloudflare Verification</DialogTitle>
-                  <DialogDescription>
-                    Please complete the captcha to confirm your airdrop request.
-                  </DialogDescription>
-                </DialogHeader>
-
-                <div className="w-full relative flex items-center justify-center p-5 py-10">
-                  <Skeleton className="absolute w-[298px] h-[62px]" />
-                  <Turnstile
-                    sitekey="0x4AAAAAAAHKo-ZE1jhM2pyN"
-                    onVerify={(token) => {
-                      requestAirdrop(token);
-                    }}
-                    refreshExpired="auto"
-                    theme="dark"
-                    className="max-w-fit rounded-lg absolute z-10"
-                  />
-                </div>
-              </DialogContent>
-            </Dialog>
-          </CardFooter>
-        </Card>
-      </div>
-
-      <div className="pointer-events-none abolute top-1/2 mb-20 ml-32 left-1/2 -translate-x-1/2 translate-y-1/2 w-52 h-28 bg-rose-600/70 blur-[120px]"></div>
-
-      {/*      <div
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-[#360568] to-[#5B2A86]">
+      <ToastContainer />
+      <div
         className="bg-[#23F0C7] text-white rounded-lg p-8 m-4 w-4/5 max-w-lg transition-all duration-500 ease-in-out transform hover:translate-y-[-2px]  drop-shadow-2xl opacity-100 hover:opacity-90"
         style={{
           boxShadow: "20px 20px 60px #1f9fa6, -20px -20px 60px #27ffb2",
@@ -298,7 +203,7 @@ export default function Home() {
             Maximum of two requests per hour.
           </p>
         </div>
-          </div>*/}
+      </div>
     </div>
   );
 }
