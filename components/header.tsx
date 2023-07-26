56557d10fd3ea3727a19f027ee2c94ec95ecfef0
Yusuf-Uluc <yusuf.uluc@protonmail.com>
2023-07-26 14:51:30 +0200

    removed wad related files

--- components/header.tsx
+++ components/header.tsx
@@ -4,8 +4,9 @@ export function Header() {
   return (
     <div className="w-full fixed top-0 left-0 flex flex-col p-10">
       <div className="flex flex-col items-center justify-center space-y-4">
-        <h1 className="text-4xl font-normal">Solana Devnet Faucet</h1>
-        <Image src="wad_logo.svg" alt="" width={60} height={20} />
+        <h1 className="text-4xl font-normal text-center">
+          Solana Devnet Faucet
+        </h1>
       </div>
     </div>
   );
