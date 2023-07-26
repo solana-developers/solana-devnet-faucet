17c904e28c6fbd34d8afecf05a8fa1cd62b27a42
Yusuf-Uluc <yusuf.uluc@protonmail.com>
2023-07-26 14:44:36 +0200

    redesign

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <div className="w-full fixed top-0 left-0 flex flex-col p-10">
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <h1 className="text-4xl font-normal">Airdrop Faucet</h1>
+        <Image src="wad_logo.svg" alt="" width={60} height={20} />
+      </div>
+    </div>
+  );
+}
