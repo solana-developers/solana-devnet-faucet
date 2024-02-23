87176486a747f43b151654912b379db741038166
nickfrosty <nfrostbutter@gmail.com>
2024-02-22 20:05:15 -0500

    fix: another one

--- next.config.js
+++ next.config.js
@@ -4,18 +4,18 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/(.*)",
+        source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://solplay.de" },
-          // Allows for specific methods accepted
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
-          // Allows for specific headers accepted (These are a few standard ones)
           {
             key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
