0ff6b3a72b70b6829de8172bfe5d9c623f523693
nickfrosty <nfrostbutter@gmail.com>
2024-02-22 20:23:18 -0500

    fix: solplay <3

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://solplay.de" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
