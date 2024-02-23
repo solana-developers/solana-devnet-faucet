e8251c4fb797a10b12e9d814afc9307e114b779c
nickfrosty <nfrostbutter@gmail.com>
2024-02-22 19:45:44 -0500

    fix: coors

--- next.config.js
+++ next.config.js
@@ -6,12 +6,7 @@ module.exports = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: " https://solplay.de" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
+          { key: "Access-Control-Allow-Origin", value: "https://solplay.de" },
         ],
       },
     ];
