a4091f399efc21b5a25927aed259517a092130db
nickfrosty <nfrostbutter@gmail.com>
2024-02-22 19:58:53 -0500

    fix: coors

--- next.config.js
+++ next.config.js
@@ -5,7 +5,19 @@ module.exports = {
       {
         // matching all API routes
         source: "/api/(.*)",
-        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
       },
     ];
   },
